0e397994d750424a1c76e77990b6201c82b352af
Konstantin Intsyn <intsyn@mail.ru>
2021-03-27 12:11:59 +0300

    Вторая игра

---
+++ Assets/Door.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    Animation animation;
+    [SerializeField]
+    float trigger_distanse = 3;
+
+    bool in_zone = false;
+    
+
+    private void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, Mathf.Infinity))
+        {
+            Control(hit);
+        }
+        else
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                Control(hit);
+            }
+    }
+
+    private void Control(RaycastHit hit)
+    {
+        if (hit.transform.gameObject.tag == "Player")
+            if (hit.distance < trigger_distanse)
+            {
+                if (!in_zone)
+                {
+                    in_zone = true;
+                    animation.Play("open");
+                }
+            }
+            else
+            {
+                if (in_zone)
+                {
+                    in_zone = false;
+                    animation.Play("close");
+                }
+            }
+        
+    }
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    a.Play("open");
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    a.Play("close");
+    //}
+}
