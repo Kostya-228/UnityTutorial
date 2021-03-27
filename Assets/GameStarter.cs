49af40864f6bc53ac3b5f32a4efe358e256c1a0d
Konstantin Intsyn <intsyn@mail.ru>
2021-03-27 17:49:18 +0300

    Пятая игра
    Добавил менюшку

---
+++ Assets/GameStarter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject GameUI;
+
+    [SerializeField]
+    GameObject PreviewCamera;
+
+    [SerializeField]
+    GameObject Player;
+
+    public void Continue()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        PreviewCamera.SetActive(false);
+        GameUI.SetActive(true);
+        Player.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
